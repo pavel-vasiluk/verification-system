9fe6db6abe5aff99b455d07eedb4e7165df980fb
Pavels Vasiluks <pavels.vasiluks@gmail.com>
2022-04-11 21:28:57 +0300

    Init symfony 6 skeleton project

--- public/index.php
+++ public/index.php
@@ -1,16 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Docker Compose Demo</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css">
-</head>
-<body>
+<?php
 
-    <h1>This is a Docker Compose Demo Page.</h1>
-    <p>This content is being served by an Nginx container.</p>
+use App\Kernel;
 
-    <?php echo 'Hello world' ?>
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-</body>
-</html>
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
