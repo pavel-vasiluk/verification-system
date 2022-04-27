56b5e3c63496011d1178d410adb67083af55f08d
Pavels Vasiluks <pavels.vasiluks@gmail.com>
2022-04-27 12:58:49 +0300

    Covered verification-confirmation scenarios with unit tests

--- tests/Service/VerificationServiceTest.php
+++ tests/Service/VerificationServiceTest.php
@@ -6,17 +6,25 @@ namespace App\Tests\Service;
 
 use App\Component\DTO\Request\VerificationSubjectDTO;
 use App\Component\DTO\Request\VerificationUserInfoDTO;
+use App\Component\Request\AbstractUserInfoAwareRequest;
+use App\Component\Request\Verification\VerificationConfirmationRequest;
 use App\Component\Request\Verification\VerificationCreationRequest;
 use App\Entity\Verification;
 use App\Enums\ConfirmationTypes;
 use App\Exception\DuplicatedVerificationException;
+use App\Exception\InvalidVerificationCodeException;
+use App\Exception\VerificationAccessDeniedException;
+use App\Exception\VerificationExpiredException;
+use App\Exception\VerificationNotFoundException;
 use App\Helper\VerificationCodeGenerationHelper;
 use App\Message\Verification\VerificationCreatedMessage;
 use App\Repository\VerificationRepository;
 use App\Service\VerificationService;
 use App\Tests\AbstractWebTestCase;
+use Carbon\Carbon;
 use JetBrains\PhpStorm\ArrayShape;
 use JsonException;
+use Ramsey\Uuid\Uuid;
 use ReflectionClass;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -86,6 +94,104 @@ class VerificationServiceTest extends AbstractWebTestCase
         $this->assertEmpty($this->messageTransport->get());
     }
 
+    public function testVerificationConfirmation(): void
+    {
+        $existingVerification = $this->prepareVerification();
+
+        $this->assertFalse($existingVerification->isConfirmed());
+        $this->assertFalse($existingVerification->isExpired());
+
+        $verificationCreationRequest = $this->prepareVerificationConfirmationRequest(
+            ['code' => $existingVerification->getCode()],
+            ['id' => $existingVerification->getId()->toString()],
+        );
+
+        $this->verificationService->confirmVerification($verificationCreationRequest);
+
+        $this->assertTrue($existingVerification->isConfirmed());
+        $this->assertTrue($existingVerification->isExpired());
+    }
+
+    public function testExceptionThrownWhenTryingToConfirmNonExistingVerification(): void
+    {
+        $verificationCreationRequest = $this->prepareVerificationConfirmationRequest(
+            ['code' => VerificationCodeGenerationHelper::generateVerificationCode(8)],
+            ['id' => Uuid::uuid4()->toString()],
+        );
+
+        $this->expectException(VerificationNotFoundException::class);
+
+        $this->verificationService->confirmVerification($verificationCreationRequest);
+    }
+
+    public function testExceptionThrownWhenTryingToConfirmVerificationWithDifferentClient(): void
+    {
+        $existingVerification = $this->prepareVerification();
+
+        $verificationCreationRequest = $this->prepareVerificationConfirmationRequest(
+            ['code' => $existingVerification->getCode()],
+            ['id' => $existingVerification->getId()->toString()],
+            array_merge(self::REQUEST_USER_INFO, ['clientIp' => '0.0.0.1'])
+        );
+
+        $this->expectException(VerificationAccessDeniedException::class);
+
+        $this->verificationService->confirmVerification($verificationCreationRequest);
+    }
+
+    public function testExceptionThrownWhenTryingToConfirmExpiredVerification(): void
+    {
+        $existingVerification = $this->prepareVerification();
+        $existingVerification->setIsExpired(true);
+
+        $this->entityManager->flush();
+
+        $verificationCreationRequest = $this->prepareVerificationConfirmationRequest(
+            ['code' => $existingVerification->getCode()],
+            ['id' => $existingVerification->getId()->toString()],
+        );
+
+        $this->expectException(VerificationExpiredException::class);
+
+        $this->verificationService->confirmVerification($verificationCreationRequest);
+    }
+
+    public function testExceptionThrownWhenTryingToConfirmExpiredByTimeVerification(): void
+    {
+        $existingVerification = $this->prepareVerification();
+
+        Carbon::setTestNow(Carbon::now()->addHour());
+
+        $verificationCreationRequest = $this->prepareVerificationConfirmationRequest(
+            ['code' => $existingVerification->getCode()],
+            ['id' => $existingVerification->getId()->toString()],
+        );
+
+        $this->expectException(VerificationExpiredException::class);
+
+        $this->verificationService->confirmVerification($verificationCreationRequest);
+    }
+
+    public function testExceptionThrownWhenTryingToConfirmVerificationWithInvalidVerificationCode(): void
+    {
+        $existingVerification = $this->prepareVerification();
+
+        $this->assertSame(0, $existingVerification->getConfirmationAttempts());
+
+        $verificationCreationRequest = $this->prepareVerificationConfirmationRequest(
+            ['code' => VerificationCodeGenerationHelper::generateVerificationCode(8)],
+            ['id' => $existingVerification->getId()->toString()],
+        );
+
+        $this->expectException(InvalidVerificationCodeException::class);
+
+        try {
+            $this->verificationService->confirmVerification($verificationCreationRequest);
+        } finally {
+            $this->assertSame(1, $existingVerification->getConfirmationAttempts());
+        }
+    }
+
     #[ArrayShape(['email notification' => 'array', 'sms notification' => 'array'])]
     public function verificationCreationDataProvider(): array
     {
@@ -128,18 +234,48 @@ class VerificationServiceTest extends AbstractWebTestCase
      */
     private function prepareVerificationCreationRequest(array $requestBody): VerificationCreationRequest
     {
-        $request = new Request();
-        $request->initialize([], [], [], [], [], [], json_encode($requestBody, JSON_THROW_ON_ERROR));
-
+        $request = $this->prepareHttpRequestWithBody($requestBody);
         $verificationCreationRequest = new VerificationCreationRequest($request);
 
-        $reflectionClass = new ReflectionClass($verificationCreationRequest);
-        $reflectionProperty = $reflectionClass->getProperty('userInfo');
-        $reflectionProperty->setValue(
-            $verificationCreationRequest,
-            new VerificationUserInfoDTO(self::REQUEST_USER_INFO)
-        );
+        $this->setRequestUserInfo($verificationCreationRequest);
 
         return $verificationCreationRequest;
+    }
+
+    /**
+     * @throws JsonException
+     */
+    private function prepareVerificationConfirmationRequest(
+        array $requestBody,
+        array $attributes = [],
+        array $userInfo = []
+    ): VerificationConfirmationRequest {
+        $request = $this->prepareHttpRequestWithBody($requestBody, $attributes);
+        $verificationConfirmationRequest = new VerificationConfirmationRequest($request);
+
+        $this->setRequestUserInfo($verificationConfirmationRequest, $userInfo);
+
+        return $verificationConfirmationRequest;
+    }
+
+    /**
+     * @throws JsonException
+     */
+    private function prepareHttpRequestWithBody(array $requestBody, array $attributes = []): Request
+    {
+        $request = new Request();
+        $request->initialize([], [], $attributes, [], [], [], json_encode($requestBody, JSON_THROW_ON_ERROR));
+
+        return $request;
+    }
+
+    private function setRequestUserInfo(AbstractUserInfoAwareRequest $request, array $userInfo = []): void
+    {
+        $reflectionClass = new ReflectionClass($request);
+        $reflectionProperty = $reflectionClass->getProperty('userInfo');
+        $reflectionProperty->setValue(
+            $request,
+            new VerificationUserInfoDTO($userInfo ?: self::REQUEST_USER_INFO)
+        );
     }
 }
